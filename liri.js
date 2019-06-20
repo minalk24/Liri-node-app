6c521b28ff58bfbc01016bcaab9382d52c9bf3af
Minal Kharche <minal.berkeley@gmail.com>
2019-06-20 07:10:38 -0700

    added code for bands in town in movies function

--- liri.js
+++ liri.js
@@ -11,46 +11,77 @@ var Spotify = require("node-spotify-api");
 
 
 //require variable
-// code required to import the keys.js file and store it in a variable
-var keys = require("./keys.js");
-// to access your keys information
-var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");  // code required to import the keys.js file and store it in a variable
+var spotify = new Spotify(keys.spotify); // to access your keys information
 
-var command = process.argv[2];
-var search = process.argv[3];
+// movies function
+var movies = function () {
 
-var movieName = process.argv.slice(2).join("+");
-console.log(movieName);
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Which movie do you want to know about",
+                name: "NameOfMovie"
+            }
+        ]).then(function (resultMovie) {
 
-queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
-// Then run a request with axios to the OMDB API with the movie specified
-axios.get(queryURL)
-.then(
-  function(response) {
-    
-      console.log("Title of the movie: " + response.data.Title + "\n\n" +
-      "Year the movie came out: " + response.data.Year + "\n\n" +
-      "IMDB Rating of the movie: " + response.data.Rated + "\n\n" +
-      "Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value + "\n\n" +
-      "Country where the movie was produced: " + response.data.Country + "\n\n" +
-      "Language of the movie: " + response.data.Language + "\n\n" +
-      "Plot of the movie: " + response.data.Plot + "\n\n" +
-      "Actors in the movie: " + response.data.Actors + "\n\n" 
-      );
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-     
-      
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+            if (resultMovie.NameOfMovie) {
+                var queryURL = "http://www.omdbapi.com/?t=" + resultMovie.NameOfMovie.split(" ").join("+") + "&y=&plot=short&apikey=trilogy";
+                console.log(queryURL);
+            }
+            Í
+            axios.get(queryURL)
+                .then(
+                    function (responseMovie) {
+
+                        console.log("Title of the movie: " + responseMovie.data.Title + "\n\n" +
+                            "Year the movie came out: " + responseMovie.data.Year + "\n\n" +
+                            "IMDB Rating of the movie: " + responseMovie.data.Rated + "\n\n" +
+                            "Rotten Tomatoes Rating of the movie: " + responseMovie.data.Ratings[1].Value + "\n\n" +
+                            "Country where the movie was produced: " + responseMovie.data.Country + "\n\n" +
+                            "Language of the movie: " + responseMovie.data.Language + "\n\n" +
+                            "Plot of the movie: " + responseMovie.data.Plot + "\n\n" +
+                            "Actors in the movie: " + responseMovie.data.Actors + "\n\n"
+                        );
+                    })
+                .catch(function () {
+
+                    console.log("Enter Correct movie Name");
+
+                    if (NameOfMovie === "Mr. Nobody") {
+                        console.log("If you havn't watch this movie, Watch it")
+                    }
+                });
+        });
+}
+
+// band In town function
+var bandsintown = function () {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Which band DO you want?",
+                name: "artist"
+            }
+        ])
+        .then(function (inquirerResponse) {
+            if (inquirerResponse.artist) {
+                var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+            }
+            axios.get(queryURL)
+                .then(function (responseAxios) {
+                    if (responseAxios.data.length === 0) {
+                        console.log("Sorry Enter correct Band Name or Artist Name");
+                    } else {
+                        for (var i = 0; i < responseAxios.data.length; i++) {
+                            console.log(" The name of the venue is :" + responseAxios.data[i].venue.name + "\n\n" +
+                                "Venue location : " + responseAxios.data[i].venue.city + "\n\n" +
+                                "Date of the Event" + moment(responseAxios.data[i].datetime).format("MM/DD/YYYY HH:mm A")
+                            );
+                        } // for loop close
+                    } //else close
+                }) //promise for axios close
+        }); //promise for inquirer close
+} //bandsInTown function close
