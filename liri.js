c5c9c64175e2e36478acfe0420fd48820985c3be
Minal Kharche <minal.berkeley@gmail.com>
2019-06-19 21:29:09 -0700

    Displayed movie information using axios with OMDB API

--- liri.js
+++ liri.js
@@ -16,17 +16,41 @@ var keys = require("./keys.js");
 // to access your keys information
 var spotify = new Spotify(keys.spotify);
 
-// Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-      {
-    // Here we create a basic text prompt.
+var command = process.argv[2];
+var search = process.argv[3];
+
+var movieName = process.argv.slice(2).join("+");
+console.log(movieName);
+
+queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get(queryURL)
+.then(
+  function(response) {
     
-  },
-   
-    
-  ])
-  .then(function(inquirerResponse) {
-    
- 
+      console.log("Title of the movie: " + response.data.Title + "\n\n" +
+      "Year the movie came out: " + response.data.Year + "\n\n" +
+      "IMDB Rating of the movie: " + response.data.Rated + "\n\n" +
+      "Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value + "\n\n" +
+      "Country where the movie was produced: " + response.data.Country + "\n\n" +
+      "Language of the movie: " + response.data.Language + "\n\n" +
+      "Plot of the movie: " + response.data.Plot + "\n\n" +
+      "Actors in the movie: " + response.data.Actors + "\n\n" 
+      );
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+     
+      
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
   });
